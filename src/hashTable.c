7981ec6959d1fead8b299c1034191ccf717c7019
yashsinghcodes <yash9vardhan@gmail.com>
2023-12-14 18:30:26 +0000

    small refactor after test

--- src/hashTable.c
+++ src/hashTable.c
@@ -1,121 +1,104 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "hashTable.h"
 
 static htItem HT_DELETED_ITEM = {NULL, NULL};
 
-static htItem* htNewItem(const char* k, char* v)
-{
-    htItem* newItem = malloc(sizeof(htItem));
-    newItem->key = k;
-    newItem->value = v;
-    return newItem;
+static htItem *htNewItem(char *k, char *v) {
+  htItem *newItem = malloc(sizeof(htItem));
+  newItem->key = k;
+  newItem->value = v;
+  return newItem;
 }
 
-hashTable* htNew()
-{
-    hashTable* newHashTable = malloc(sizeof(hashTable));
+hashTable *htNew() {
+  hashTable *newHashTable = malloc(sizeof(hashTable));
 
-    newHashTable->size = 53;
-    newHashTable->count = 0;
-    newHashTable->items = calloc((size_t)newHashTable->size, sizeof(htItem*));
-    return newHashTable;
+  newHashTable->size = 53;
+  newHashTable->count = 0;
+  newHashTable->items = calloc((size_t)newHashTable->size, sizeof(htItem *));
+  return newHashTable;
 }
 
-static void htDelItem(htItem* i)
-{
-    free((void *)i->key);
-    free(i->value);
-    free(i);
-}
+static void htDelItem(htItem *i) { free(i); }
 
-void delHashTable(hashTable* ht)
-{
-    for (int i = 0; i < ht->size; i++) {
-        htItem* item = ht->items[i];
-        if (item != NULL) {
-           htDelItem(item);
-        }
+void delHashTable(hashTable *ht) {
+  for (int i = 0; i < ht->size; i++) {
+    htItem *item = ht->items[i];
+    if (item != NULL) {
+      htDelItem(item);
     }
-    free(ht->items);
-    free(ht);
+  }
+  free(ht->items);
+  free(ht);
 }
 
-static int hashFunc(const char* s, const int primeNum, const int hashTableLen)
-{
-    long hash = 0;
-    const int lenKey = strlen(s);
-    for (int i=0; i < lenKey; i++) {
-        hash += (long)pow(primeNum, lenKey- (i+1)) * (int)s[i];
-        hash = hash % hashTableLen;
-    }
-    return (int)hash;
+static int hashFunc(const char *s, const int primeNum, const int hashTableLen) {
+  long hash = 0;
+  const int lenKey = strlen(s);
+  for (int i = 0; i < lenKey; i++) {
+    hash += (long)pow(primeNum, lenKey - (i + 1)) * (int)s[i];
+    hash = hash % hashTableLen;
+  }
+  return (int)hash;
 }
 
 // index = (hash_a(string) + i * (hash_b(string) + 1)) % num_buckets
 
-static int getHash(const char* s, const int hashTableLen, const int attempt)
-{
-    const int hash_a = hashFunc(s, PRIME_NUM1, hashTableLen);
-    const int hash_b = hashFunc(s, PRIME_NUM2, hashTableLen);
+static int getHash(const char *s, const int hashTableLen, const int attempt) {
+  const int hash_a = hashFunc(s, PRIME_NUM1, hashTableLen);
+  const int hash_b = hashFunc(s, PRIME_NUM2, hashTableLen);
 
-    return (hash_a + (attempt * (hash_b + 1))) % hashTableLen;
+  return (hash_a + (attempt * (hash_b + 1))) % hashTableLen;
 }
 
-
-void htInsert(hashTable *ht, const char *key, char *value)
-{
-    htItem* item = htNewItem(key, value);
-    int index = getHash(item->key, ht->size, 0);
-    htItem* curItem = ht->items[index];
-    for (int i = 1; curItem != NULL && curItem != &HT_DELETED_ITEM; ++i)
-    {
-        if (strcmp(curItem->key, key) == 0) {
-            htDelItem(curItem);
-            ht->items[index] = item;
-            return;
-        }
-        index = getHash(item->key, ht->size, i);
-        curItem = ht->items[index];
+void htInsert(hashTable *ht, char *key, char *value) {
+  htItem *item = htNewItem(key, value);
+  int index = getHash(item->key, ht->size, 0);
+  htItem *curItem = ht->items[index];
+  for (int i = 1; curItem != NULL && curItem != &HT_DELETED_ITEM; ++i) {
+    if (strcmp(curItem->key, key) == 0) {
+      htDelItem(curItem);
+      ht->items[index] = item;
+      return;
     }
-    ht->items[index] = item;
-    ht->count++;
+    index = getHash(item->key, ht->size, i);
+    curItem = ht->items[index];
+  }
+  ht->items[index] = item;
+  ht->count++;
 }
 
-char* htSearch(hashTable *ht, const char* key)
-{
-    int index = getHash(key, ht->size, 0);
-    htItem* item = ht->items[index];
-    for (int i=1; item != NULL; ++i) {
-        if (item != &HT_DELETED_ITEM)
-            if (*key == *item->key && 
-                !strcmp(key, item->key)) {
-                return item->value;
-            }
-        index = getHash(key, ht->size, i);
-        item = ht->items[index];
-    }
-    return NULL;
+char *htSearch(hashTable *ht, char *key) {
+  int index = getHash(key, ht->size, 0);
+  htItem *item = ht->items[index];
+  for (int i = 1; item != NULL; ++i) {
+    if (item != &HT_DELETED_ITEM)
+      if (*key == *item->key && !strcmp(key, item->key)) {
+        return item->value;
+      }
+    index = getHash(key, ht->size, i);
+    item = ht->items[index];
+  }
+  return NULL;
 }
 
+void htDelete(hashTable *ht, char *key) {
+  int index = getHash(key, ht->size, 0);
+  htItem *item = ht->items[index];
 
-void htDelete(hashTable *ht, const char *key) 
-{
-    int index = getHash(key, ht->size, 0);
-    htItem* item = ht->items[index];
-    
-    for (int i=1; item != NULL; ++i) {
-        if (item != &HT_DELETED_ITEM) {
-            if (strcmp(key, item->key) == 0){
-                htDelItem(item); 
-                ht->items[index] = &HT_DELETED_ITEM;
-                ht->count--;
-            }
-        }
-        index = getHash(key, ht->size, i);
-        item = ht->items[index];
+  for (int i = 1; item != NULL; ++i) {
+    if (item != &HT_DELETED_ITEM) {
+      if (strcmp(key, item->key) == 0) {
+        htDelItem(item);
+        ht->items[index] = &HT_DELETED_ITEM;
+        ht->count--;
+      }
     }
+    index = getHash(key, ht->size, i);
+    item = ht->items[index];
+  }
 }
--- src/hashTable.h
+++ src/hashTable.h
@@ -1,20 +1,19 @@
 #define PRIME_NUM1 151
 #define PRIME_NUM2 163
 
+typedef struct {
+  char *key;
+  char *value;
+} htItem;
 
 typedef struct {
-    const char* key;
-    char* value;
-}htItem;
+  int size;
+  int count;
+  htItem **items;
+} hashTable;
 
-typedef struct {
-    int size;
-    int count;
-    htItem** items;  
-}hashTable;
-
-hashTable* htNew();
-void delHashTable(hashTable*);
-void htInsert(hashTable* ht, const char* key, char* value);
-char* htSearch(hashTable* ht, const char* key);
-void htDelete(hashTable* ht, const char* key);
+hashTable *htNew();
+void delHashTable(hashTable *);
+void htInsert(hashTable *ht, char *key, char *value);
+char *htSearch(hashTable *ht, char *key);
+void htDelete(hashTable *ht, char *key);
